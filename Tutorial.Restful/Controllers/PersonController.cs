7fc4db4e6ca2b87945282043f2ef16d74453daff
陈海荣 <chenhairong@sunlight.bz>
2018-06-13 14:53:42 +0800

    增加 Swagger界面

--- Tutorial.Restful/Controllers/PersonController.cs
+++ Tutorial.Restful/Controllers/PersonController.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Tutorial.Restful.Controllers
 {
     [Route("api/[controller]s")]
     public class PersonController : Controller
     {
-        // GET
-        public string Index()
+        /// <summary>
+        /// 获取人员信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<string> Index(int id)
         {
-            return "person";
+            return Ok("person");
+        }
+
+        [NonAction]
+        public ActionResult<DateTime> GetTime()
+        {
+            return Ok(DateTime.Now);
         }
     }
 }
--- Tutorial.Restful/Startup.cs
+++ Tutorial.Restful/Startup.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using NJsonSchema;
+using NSwag.AspNetCore;
+using System.Reflection;
 
 namespace Tutorial.Restful
 {
@@ -25,6 +28,22 @@ namespace Tutorial.Restful
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
+
+            // 开启Swagger UI 中间件和 Swagger generator
+
+            app.UseSwaggerUi3(typeof(Startup).GetTypeInfo().Assembly, setting =>
+                {
+                    setting.GeneratorSettings.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
+                    setting.PostProcess = document =>
+                    {
+                        document.Info.Version = "v1";
+                        document.Info.Title = "Tutorial Restful API";
+                        document.Info.Description = "Restful API 的示例";
+                        document.Info.TermsOfService = "None";
+                    };
+                });
 
             app.UseMvc();
 
