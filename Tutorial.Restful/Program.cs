c3dadbb2c40cafcac7dff6c9cc9a7de0ac16bb99
陈海荣 <chenhairong@sunlight.bz>
2018-06-13 17:35:00 +0800

    项目配置

--- Tutorial.Restful/Program.cs
+++ Tutorial.Restful/Program.cs
@@ -12,13 +12,39 @@ namespace Tutorial.Restful
 {
     public class Program
     {
+        public static IConfiguration Configuration { get; set; }
+
         public static void Main(string[] args)
         {
+            DealWithConfiguration();
+
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+
+        public static void DealWithConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("FirstConfig.json")
+                .AddJsonFile("SecondConfig.json");
+
+            Configuration = configBuilder.Build();
+
+            foreach (var item in Configuration.AsEnumerable())
+            {
+                System.Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+
+            System.Console.WriteLine($"Key1: {Configuration["key1"]}");
+            System.Console.WriteLine($"Key2: {Configuration["key2"]}");
+            System.Console.WriteLine($"childkey1: {Configuration["key3:childkey1"]}");
+
+            var key3Section = Configuration.GetSection("key3");
+            Console.WriteLine($"key3:childkey1: {key3Section["childkey1"]}");
+        }
     }
 }
