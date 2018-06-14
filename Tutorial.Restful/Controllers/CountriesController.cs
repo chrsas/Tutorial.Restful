67dda1dedb5a5a845d98011b98dc0eae3814de5e
陈海荣 <chenhairong@sunlight.bz>
2018-06-14 11:30:02 +0800

    调整Controller，不暴露Dto

---
+++ Tutorial.Restful/Controllers/CountriesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tutorial.Restful.Controllers.Dto;
+using Tutorial.Restful.Data;
+using Tutorial.Restful.Domain.Models;
+using System.Linq;
+
+namespace Tutorial.Restful.Host.Controllers
+{
+    [Route("api/[controller]")]
+    public class CountriesController : Controller
+    {
+        private readonly RestfulContext _restfulContext;
+
+        public CountriesController(RestfulContext restfulContext)
+        {
+            _restfulContext = restfulContext;
+        }
+
+        public async Task<IEnumerable<CountryDto>> Get()
+        {
+            return await _restfulContext.Countries.Select(c => new CountryDto
+            {
+                Id = c.Id,
+                Abbreviation = c.Abbreviation,
+                ChineseName = c.ChineseName,
+                Continent = c.Continent,
+                EnglishName = c.EnglishName
+            }).ToListAsync();
+        }
+    }
+}
---
+++ Tutorial.Restful/Controllers/Dto/CountryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Tutorial.Restful.Controllers.Dto
+{
+    public class CountryDto
+    {
+        public int Id { get; set; }
+        public string EnglishName { get; set; }
+        public string ChineseName { get; set; }
+        public string Abbreviation { get; set; }
+        public string Continent { get; set; }
+    }
+}
